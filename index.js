27b7ae3607907c07ccd8ef2c0c2a8748ec48ce3f
Tanimismat <tanim.ismat@gmail.com>
2022-05-08 22:12:41 +0600

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -13,4 +14,15 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log('Listening to port', port);
-})
+});
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sidhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('warehouse DB connected')
+    // perform actions on the collection object
+    client.close();
+});
