339aa52cf284a51d6060feb3310e5240905d1710
Tanimismat <tanim.ismat@gmail.com>
2022-05-09 11:41:30 +0600

    load data

--- index.js
+++ index.js
@@ -20,9 +20,27 @@ app.listen(port, () => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sidhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('warehouse DB connected')
-    // perform actions on the collection object
-    client.close();
-});
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('warehouse DB connected')
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+async function run() {
+    try {
+        await client.connect();
+        const carCollection = client.db('warehouseManagement').collection('car');
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = carCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
